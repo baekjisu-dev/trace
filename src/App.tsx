434b72d68be84a8d90d8b7f6135d4d41e3161479
baekjisu-dev <jisu.dev6476@gmail.com>
2026-01-13 21:47:07 +0900

    feat: dark/light/system theme 기능을 위한 코드 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import RootRoute from "./root-route";
+import { ThemeProvider } from "./provider/theme-provider";
 
 function App() {
-  return <RootRoute />;
+  return (
+    <ThemeProvider>
+      <RootRoute />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/lib/theme-context.ts
@@ -0,0 +1,22 @@
+import { createContext, useContext } from "react";
+
+export type Theme = "dark" | "light" | "system";
+
+export type ThemeProviderState = {
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
+};
+
+export const createThemeProviderState = (): ThemeProviderState => ({
+  theme: "system",
+  setTheme: () => null,
+});
+
+export const ThemeProviderContext = createContext<ThemeProviderState>(
+  createThemeProviderState()
+);
+
+export const useTheme = (): ThemeProviderState => {
+  const context = useContext(ThemeProviderContext);
+  return context;
+};
