539a13d045f8a19ee8c0d09512cd5debc74ef0fc
baekjisu-dev <jisu.dev6476@gmail.com>
2026-01-17 18:36:35 +0900

    feat: sidebar nav 컴포넌트 작성

--- src/components/layout/nav/sidebar-nav.tsx
+++ src/components/layout/nav/sidebar-nav.tsx
@@ -1,4 +1,7 @@
+import { Button } from "@/components/ui/button";
 import { NAV_ITEMS } from "@/lib/nav-items";
+import { cn } from "@/lib/utils";
+import { Link } from "react-router";
 
 
 interface SidebarNavProps {
@@ -7,7 +10,16 @@ interface SidebarNavProps {
 
 const SidebarNav = ({ activeNavKey }: SidebarNavProps) => {
   return (
-    <aside className="hidden md:block w-64 h-full border-r">sidebar-nav</aside>
+    <nav className="hidden md:flex w-40 h-full border-r flex-col gap-2 px-2 py-4">
+      {NAV_ITEMS.map((item) => (
+        <Link className="w-full box-border" key={item.key} to={item.href}>
+          <Button variant="ghost" className={cn("w-full justify-start text-muted-foreground gap-1", activeNavKey === item.key && "text-primary")}>
+            <item.icon className={cn("size-4 stroke-2", activeNavKey === item.key && "stroke-3")} />
+            <span className={cn("text-xs", activeNavKey === item.key && "font-bold")}>{item.label}</span>
+          </Button>
+        </Link>
+      ))}
+    </nav>
   )
 }
 
