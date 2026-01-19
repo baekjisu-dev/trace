2e3368bd5fdf954452f4591eaac2d879f498e200
baekjisu-dev <jisu.dev6476@gmail.com>
2026-01-19 22:54:20 +0900

    feat: tiptap 에디터 적용 중 커밋

--- src/components/post/post-editor.tsx
+++ src/components/post/post-editor.tsx
@@ -1,6 +1,27 @@
+import '@/components/post/styles.scss'
+
+import { EditorContent, useEditor } from '@tiptap/react';
+import { TextStyleKit } from '@tiptap/extension-text-style'
+import StarterKit from '@tiptap/starter-kit'
+import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight'
+import { all, createLowlight } from 'lowlight'
+import Code from '@tiptap/extension-code';
+import Highlight from '@tiptap/extension-highlight';
+
+const lowlight = createLowlight(all)
+
+const extensions = [TextStyleKit, StarterKit, CodeBlockLowlight.configure({ lowlight }), Code, Highlight]
+
 const PostEditor = () => {
+  const editor = useEditor({
+    extensions,
+    content: '',
+  });
+
   return (
-    <div>post-editor</div>
+    <div className="p-2.5 border-b">
+      <EditorContent editor={editor} />
+    </div>
   )
 }
 
--- src/pages/index-page.tsx
+++ src/pages/index-page.tsx
@@ -1,8 +1,10 @@
+import PostEditor from "@/components/post/post-editor";
 import PostItem from "@/components/post/post-item";
 
 const IndexPage = () => {
   return (
     <div className="h-full w-full">
+      <PostEditor />
       <section className="w-full h-full p-2.5">
         <PostItem />
       </section>
