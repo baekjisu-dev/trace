4ba84fe9a6afc8628ea08fd296cc436b037103c3
baekjisu-dev <jisu.dev6476@gmail.com>
2026-01-21 22:03:19 +0900

    feat: 텍스트 에디터 컴포넌트 개발

--- src/components/post/post-editor.tsx
+++ src/components/post/post-editor.tsx
@@ -1,16 +1,35 @@
 import '@/components/post/styles.scss'
+import '@/components/tiptap-node/blockquote-node/blockquote-node.scss'
+import '@/components/tiptap-node/code-block-node/code-block-node.scss'
+import '@/components/tiptap-node/heading-node/heading-node.scss'
+import '@/components/tiptap-node/horizontal-rule-node/horizontal-rule-node.scss'
+import '@/components/tiptap-node/list-node/list-node.scss'
+import '@/components/tiptap-node/paragraph-node/paragraph-node.scss'
 
-import { EditorContent, useEditor } from '@tiptap/react';
+import { EditorContent, EditorContext, useEditor } from '@tiptap/react';
 import { TextStyleKit } from '@tiptap/extension-text-style'
 import StarterKit from '@tiptap/starter-kit'
 import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight'
 import { all, createLowlight } from 'lowlight'
 import Code from '@tiptap/extension-code';
 import Highlight from '@tiptap/extension-highlight';
+import TextAlign from '@tiptap/extension-text-align'
+import { BlockquoteButton } from '@/components/tiptap-ui/blockquote-button';
+import { ColorHighlightPopover } from '@/components/tiptap-ui/color-highlight-popover';
+import { LinkPopover } from '@/components/tiptap-ui/link-popover';
+import { HeadingDropdownMenu } from '@/components/tiptap-ui/heading-dropdown-menu';
+import { Underline } from '@tiptap/extension-underline'
+import { Superscript } from '@tiptap/extension-superscript'
+import { Subscript } from '@tiptap/extension-subscript'
+import { MarkButton } from '@/components/tiptap-ui/mark-button';
+
+import { Button } from '@/components/ui/button';
+import { BookOpenTextIcon, ImagePlusIcon } from 'lucide-react'
+import TooltipWrapper from '@/components/ui/tooltip-wrapper'
 
 const lowlight = createLowlight(all)
 
-const extensions = [TextStyleKit, StarterKit, CodeBlockLowlight.configure({ lowlight }), Code, Highlight]
+const extensions = [TextStyleKit, StarterKit, CodeBlockLowlight.configure({ lowlight }), Code, Highlight.configure({multicolor: true}), Underline, Superscript, Subscript, TextAlign.configure({ types: ['heading', 'paragraph'] })]
 
 const PostEditor = () => {
   const editor = useEditor({
@@ -19,8 +38,37 @@ const PostEditor = () => {
   });
 
   return (
-    <div className="p-2.5 border-b">
-      <EditorContent editor={editor} />
+    <div className="w-full border-b">
+      <EditorContext.Provider value={{ editor }}>
+        <div className="flex items-center gap-1 w-full overflow-auto border-b px-2.5 py-1">
+          <HeadingDropdownMenu levels={[1, 2, 3, 4]} />
+          <BlockquoteButton  />
+          <div className='w-px h-4 bg-gray-200' />
+          <MarkButton type="bold" />
+          <MarkButton type="italic" />
+          <MarkButton type="strike" />
+          <MarkButton type="underline" />
+          <div className='w-px h-4 bg-gray-200' />
+          <ColorHighlightPopover  />
+          <LinkPopover  />
+        </div>
+        <EditorContent className="max-h-[200px] overflow-auto" editor={editor} />
+      </EditorContext.Provider>
+      <div className="w-full p-2.5 flex items-center justify-between">
+        <div className="flex items-center gap-1">
+          <TooltipWrapper tooltip="이미지">
+            <Button className="rounded-full" size="icon" variant="outline">
+              <ImagePlusIcon className="size-4" />
+            </Button>
+          </TooltipWrapper>
+          <TooltipWrapper tooltip="책 정보">
+            <Button className="rounded-full" size="icon" variant="outline">
+              <BookOpenTextIcon className="size-4" />
+            </Button>
+          </TooltipWrapper>
+        </div>
+        <Button>게시하기</Button>
+      </div>
     </div>
   )
 }
---
+++ src/components/ui/tooltip-wrapper.tsx
@@ -0,0 +1,21 @@
+import { Tooltip, TooltipContent, TooltipTrigger } from "./tooltip"
+
+interface TooltipWrapperProps {
+  tooltip: string
+  children: React.ReactNode
+}
+
+const TooltipWrapper = ({ tooltip, children }: TooltipWrapperProps) => {
+  return (
+    <Tooltip>
+      <TooltipTrigger>
+        {children}
+      </TooltipTrigger>
+      <TooltipContent>
+        <p>{tooltip}</p>
+      </TooltipContent>
+    </Tooltip>
+  )
+}
+
+export default TooltipWrapper
